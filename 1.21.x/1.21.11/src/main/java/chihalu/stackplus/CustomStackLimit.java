b2b64cc52b5a0a440a390b3f452ea0be3a4d02c1
Chihalu <m.chihiro0314@gmail.com>
2026-07-11 02:11:38 +0900

    Set up 1.21.4 dedicated project

---
+++ 1.21.x/1.21.11/src/main/java/chihalu/stackplus/CustomStackLimit.java
@@ -0,0 +1,18 @@
+package chihalu.stackplus;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CustomStackLimit implements ModInitializer {
+	public static final String MOD_ID = "stackplus";
+
+	// このロガーはコンソールとログファイルにテキストを書き込むために使用されます。
+	// ロガーの名前にMod IDを使用することがベストプラクティスとされています。
+	// こうすることで、どのModが情報、警告、エラーを出力したかが明確になります。
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
